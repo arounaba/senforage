add6dd4d9b104ecc6eead86aeca9a4eee5f92579
dev <arounaba48@gmail.comgit>
2019-05-28 19:18:17 +0000

    ajoute RolesTableSeeder

--- config/app.php
+++ config/app.php
@@ -167,14 +167,14 @@ return [
          */
 
         /*
-         * Application Service Providers...
+          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Reliese\Coders\CodersServiceProvider::class,
     ],
 
     /*
@@ -192,7 +192,7 @@ return [
 
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
+        'Artisan' => Illuminate\Support\Facades\Artisan::class,       
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
