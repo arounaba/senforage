cfb74e81dc43102d27022e17d6bb1258af579a59
dev <arounaba48@gmail.comgit>
2019-06-26 21:23:27 +0000

    26:06:2019

---
+++ database/seeds/ConsommationsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConsommationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Consommation::class,100)->create(['factures_id'=>null]);
+    }
+}
